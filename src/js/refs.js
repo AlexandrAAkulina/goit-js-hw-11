2fb07577f45315869ee8ea93668ef5a177aec7f8
AlexandrAAkulina <icomfy1484@gmail.com>
2023-11-01 11:21:06 +0200

    update

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,6 +1,6 @@
 export const refs = {
   galleryEl: document.querySelector('.gallery'),
-  searchInput: document.querySelector('input[name="searchQuery"'),
+  searchInput: document.querySelector('input[name="searchQuery"]'),
   searchForm: document.getElementById('search-form'),
   loaderEl: document.querySelector('.loader'),
 };
